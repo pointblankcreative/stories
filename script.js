d5335d6e882222a4b1d8f35e4c5e4c51bd46528f
AdrienneLee <adrienneklee@gmail.com>
2022-04-27 16:45:43 -0400

    pagination added

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-$('.button').click(function(){
+$(document).ready(function(){
+  $(".content").slice(0, 4).show();
+  $("#loadMore").on("click", function(e){
+    e.preventDefault();
+    $(".content:hidden").slice(0, 4).slideDown();
+    if($(".content:hidden").length == 0) {
+      $("#loadMore").text("Share stories").addClass("noContent");
+    }
+  });
+
+  $('.button').click(function(){
   const buttonId = $(this).attr('id');
   $('#modal-container').removeAttr('class').addClass(buttonId);
   $('body').addClass('modal-active');
+  })
+
+  $('#modal-container').click(function(){
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+  });
+  
 })
 
-$('#modal-container').click(function(){
-  $(this).addClass('out');
-  $('body').removeClass('modal-active');
-});
+
+
