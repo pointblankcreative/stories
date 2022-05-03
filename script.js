c8589c3b7c78a316bba1414e98873a86316a7b9b
AdrienneLee <adrienneklee@gmail.com>
2022-05-03 13:33:28 -0400

    added elipse read more to js

--- script.js
+++ script.js
@@ -29,4 +29,18 @@ window.onclick = function (e) {
   }
 }
 
+// Expand Text
+function expand() {
+  const dots = document.getElementById('dots');
+  const moreText = document.getElementById('more');
+  
+  if (dots.style.display === 'none') {
+    dots.style.display = 'inline';
+    moreText.style.display = 'none'
+  } else {
+    dots.style.display = 'none';
+    moreText.style.display = 'inline';
+  }
+}
+
 
