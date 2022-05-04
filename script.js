46e9cd419286f0cb2c0c40e967978f4cb9f30ef7
AdrienneLee <adrienneklee@gmail.com>
2022-05-04 16:13:57 -0400

    mobile header styling updated

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 $(document).ready(function(){
   // Load More
   $(".content").slice(0, 3).show();
+
   $("#loadMore").on("click", function(e){
     e.preventDefault();
     $(".content:hidden").slice(0, 3).slideDown();
+    
     if($(".content:hidden").length == 0) {
-      $("#loadMore").text("Share stories").addClass("noContent");
+      $("#loadMore").text("End of stories").addClass("noContent");
     }
   });
-  
 })
 
 // Modal Script
